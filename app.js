d45a3c7d4a5e4db3ee1e1f36865f64519d67e5ce
abdullahalhadrami <abdullahalhadrami102@gmail.com>
2020-05-12 12:02:33 +0530

    adding bycrypt

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const bycrypt=require("bycrypt");
+const saltRounds = 10;
+
+// const md5 = require('md5');
+//const encrypt=require("mongoose-encryption");
 
 const app = express();
 
@@ -24,10 +28,12 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 //create model using created schema
 const User=new mongoose.model("User",userSchema);
+
+
 
 app.get("/",function(req,res){
   res.render("home");
@@ -42,23 +48,26 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser=new User({
-    email:req.body.username,
-    password:req.body.password
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.render("secrets");
-    }
-  });
+
 });
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
   User.findOne({email:username},function(err,foundUser){
     if(err){
       console.log(err);
